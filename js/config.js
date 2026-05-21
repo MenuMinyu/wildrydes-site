f484a6893a62969cf756b8da53f21f64eaf5db2c
MenuMinyu <130168857+MenuMinyu@users.noreply.github.com>
2026-05-21 13:59:44 +0100

    Add API invoke URL in config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://7w6j4cwgif.execute-api.eu-west-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
